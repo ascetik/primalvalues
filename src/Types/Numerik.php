dead3e9989c30a49290b1da0e7f6c5e2f3e9f9bf
ascetik <ascetik@free.fr>
2024-01-19 18:35:24 +0100

    Numerik : methode multiply et divide

--- src/Types/Numerik.php
+++ src/Types/Numerik.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Ascetik\Primalvalues\Types;
 
-interface Numerik
-{
+use Ascetik\Primalvalues\Values\NonZero;
 
+interface Numerik extends PrimalValue
+{
+    public function add(int|self $number): self;
+    public function subtract(int|self $number): self;
+    public function multiply(int|self $number): self;
+    public function divide(int|NonZero $number): self;
 }
